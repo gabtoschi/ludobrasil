e40ac95b033ce6a038735c83a1da366283487e91
Gabriel Toschi <gabriel.oliveira@techne.com.br>
2026-07-01 18:52:59 -0300

    Fix file download URLs

--- src/dataset/v1/index.ts
+++ src/dataset/v1/index.ts
@@ -4,7 +4,7 @@ import { V1_RAW_DATA } from "./v1.data";
 export const DatasetV1: DatasetVersion = {
   version: 'v1',
   publishDate: '01/07/2026',
-  csvFile: 'ludobrasil/dataset/v1/ludobrasil-v1.csv',
-  jsonFile: 'ludobrasil/dataset/v1/ludobrasil-v1.json',
+  csvFile: 'dataset/v1/ludobrasil-v1.csv',
+  jsonFile: 'dataset/v1/ludobrasil-v1.json',
   data: V1_RAW_DATA,
 }
