7f4a049a3ed7027ea27ae8cf2f4d5804ff074ce5
Gabriel Toschi <gabriel.oliveira@techne.com.br>
2026-07-01 18:57:53 -0300

    Add footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import './Footer.css';
 export default function Footer() {
   return <footer className="bg-warning-subtle container-fluid py-4">
     <div className="container d-flex flex-column justify-content-center align-items-center gap-3">
-      <img className="footer-logo" src={ToschiLogo} alt='Gabriel Toschi' />
+      <a href='https://gabtoschi.com/pt' target='_blank'><img className="footer-logo" src={ToschiLogo} alt='Gabriel Toschi' /></a>
       <span className='text-center'><b>Projeto LudoBrasil</b> • {CURRENT_DATASET.version} • última atualização em {CURRENT_DATASET.publishDate}</span>
     </div>
   </footer>
